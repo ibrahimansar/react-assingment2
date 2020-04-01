cf5846b766db95d2e2ebfa27a8a6deec1b1b729c
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-04-01 20:01:55 +0530

    Add files via upload

---
+++ Validation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const validation = (props) => {
+    let validationMessage ='text long enough';
+
+    if(props.inputLength<= 5){
+        validationMessage= 'text too short';
+    }
+
+    return (
+        <div>
+          <p> {validationMessage}</p>
+        </div>
+    )
+};
+
+export default validation;
